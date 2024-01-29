384b37c48ff3b39c7b36a4025739dae831f91daf
Angela Hempkmeier <aggsdpz@gmail.com>
2024-01-29 18:38:55 -0300

    refact(app/*): changing the arch's project to learning about angular basics

--- my-first-project-latest/src/app/app.component.ts
+++ my-first-project-latest/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet],
+  template: `
+    <h1>Curso de Angular</h1>
+    <h2> Components </h2> 
+  `,
+  //template: `   <router-outlet />   `,
+  //styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-project-latest';
 }
+
