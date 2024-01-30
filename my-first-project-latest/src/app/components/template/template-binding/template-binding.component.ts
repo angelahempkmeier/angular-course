2b3b7e37af57e6202453a4bdbdb104986eb1e06a
Angela Hempkmeier <aggsdpz@gmail.com>
2024-01-30 18:19:24 -0300

    feat: property bindings

--- my-first-project-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ my-first-project-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -12,6 +12,15 @@ export class TemplateBindingComponent {
   public name: string = 'Angela Hempkmeier';
   public testWithoutType = 'teste';
   public age: number = 28;
+  public condition = this.age > 1 ? 'Maior que um' : 'Menor que um';
+  public isDisabled = true;
+  public srcValue = 'https://img.freepik.com/fotos-gratis/ceu-azul-com-nuvens_1232-936.jpg';
+  
+  constructor(){
+    setTimeout(() => {
+      this.name = 'Angie';
+    }, 1000);
+  }
 
   public sum(name:string, age:number){
     return name + age;
