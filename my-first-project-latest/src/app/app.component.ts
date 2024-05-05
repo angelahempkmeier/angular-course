320e14c06fce04da140b1da4d0406cf0d7d3b103
Angela Hempkmeier <aggsdpz@gmail.com>
2024-05-05 20:16:00 -0300

    control flow

--- my-first-project-latest/src/app/app.component.ts
+++ my-first-project-latest/src/app/app.component.ts
@@ -5,16 +5,18 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
   template: `
     <h1>Curso de Angular</h1>
     <app-template-variables />
     <app-template-binding />
     <!--<app-new-component />-->
+    <app-template-control-flow/>
   `
 })
 export class AppComponent {
---
+++ my-first-project-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-control-flow',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-control-flow.component.html',
+  styleUrl: './template-control-flow.component.scss'
+})
+export class TemplateControlFlowComponent {
+
+}
