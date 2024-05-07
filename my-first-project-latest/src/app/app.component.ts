ea66bed30f19214b60b7d59f71365552cf9235db
Angela Hempkmeier <aggsdpz@gmail.com>
2024-05-07 19:10:29 -0300

    feat: deferrable views

--- my-first-project-latest/src/app/app.component.ts
+++ my-first-project-latest/src/app/app.component.ts
@@ -6,11 +6,12 @@ import { NewComponent } from './components/new-component/new-component.component
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgIf, NgFor, AsyncPipe, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
+  imports: [CommonModule, NgIf, NgFor, AsyncPipe, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent, TemplateDeferrableViewsComponent],
   template: `
     <h1>Curso de Angular</h1>
     <app-template-variables />
---
+++ my-first-project-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-deferrable-views',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-deferrable-views.component.html',
+  styleUrl: './template-deferrable-views.component.scss'
+})
+export class TemplateDeferrableViewsComponent {
+
+}
